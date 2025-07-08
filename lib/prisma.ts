46e10104ff9720bdca0ed4d322ecdaf61b33804f
Kuntalmajee2557 <kuntalmajee338@gmail.com>
2025-07-08 12:56:59 +0530

    change in prisma global

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,7 +1,11 @@
 // lib/prisma.ts
 import { PrismaClient } from "@prisma/client";
 
-const prismaClient = globalThis.prismaClient ?? new PrismaClient();
+declare global {
+  var prisma: PrismaClient | undefined;
+}
+
+const prismaClient = globalThis.prisma ?? new PrismaClient();
 
 if (process.env.NODE_ENV !== "production") {
   globalThis.prismaClient = prismaClient;
