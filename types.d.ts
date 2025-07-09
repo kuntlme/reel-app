0526ad94e707438c5c5641f37c7b8216ac86239a
Kuntalmajee2557 <kuntalmajee338@gmail.com>
2025-07-09 20:02:10 +0530

    package updated

--- types.d.ts
+++ types.d.ts
@@ -1,15 +1,3 @@
-// import { Connection } from "mongoose"
-
-// declare global {
-//     var mongoose: {
-//         conn: Connection | null
-//         promise: Promise<Connection> | null
-//     }
-// }
-
-// export {}
-
-// types.d.ts
 import { PrismaClient } from "@prisma/client";
 
 declare global {
